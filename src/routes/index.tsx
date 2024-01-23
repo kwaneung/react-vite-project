8c39356c22c10991c413530faa451ef6ef965b14
kwaneung <kwaneung.kim@outlook.com>
2024-01-23 10:10:20 +0900

    src/routes/index.tsx init

---
+++ src/routes/index.tsx
@@ -0,0 +1,17 @@
+/**
+ * src/routes/index.tsx
+ */
+
+import {Routes} from "react-router-dom";
+
+const AppRouter = () => {
+    return (
+        <Routes>
+            {/*<Route index element={<DefaultComponent />}></Route>*/}
+            {/*<Route path='/task1' element={<Task1 />}></Route>*/}
+            {/*<Route path='/task2' element={<Task2 />}></Route>*/}
+        </Routes>
+    );
+};
+
+export default AppRouter;
