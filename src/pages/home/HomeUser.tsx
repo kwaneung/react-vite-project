f6de1a92c72a95d435e569d2330931a896749bc9
kwaneung <kwaneung.kim@outlook.com>
2024-02-10 19:06:54 +0900

    homeuser 컴포넌트 추가

---
+++ src/pages/home/HomeUser.tsx
@@ -0,0 +1,10 @@
+/**
+ * src/pages/home/HomeUser.tsx
+ */
+
+const HomeUser = () => {
+  console.log('HomeUser component!!!');
+  return <h1>HomeUser Component</h1>;
+};
+
+export default HomeUser;
--- src/routes/HomeRoute.tsx
+++ src/routes/HomeRoute.tsx
@@ -6,6 +6,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '@pages/home/Home';
 import SecondHome from '@pages/home/SecondHome';
 import FirstHome from '@pages/home/FirstHome';
+import HomeUser from '@pages/home/HomeUser';
 
 const HomeRoute = () => (
   <Routes>
@@ -13,6 +14,7 @@ const HomeRoute = () => (
       <Route path="firsthome" element={<FirstHome />} />
       <Route path="secondhome" element={<SecondHome />} />
     </Route>
+    <Route path="/homeuser" element={<HomeUser />} />
   </Routes>
 );
 export default HomeRoute;
